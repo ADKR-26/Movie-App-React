5bd64d7b10be1b12947063ab8aabddc4ae5ea950
Aaditya Kumar <adityavishwakarma2011@gmail.com>
2023-04-22 21:14:43 +0530

    Addeed Similar and Recommendation Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,6 +5,8 @@ import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import Cast from './cast/Cast';
 import VideosSection from './videosSection/VideosSection';
+import Similar from './carousels/Similar';
+import Recommendation from './carousels/Recommendation';
 
 function Details() {
 
@@ -17,6 +19,8 @@ function Details() {
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id}/>
+      <Recommendation mediaType={mediaType} id={id}/>
     </div>
   )
 }
