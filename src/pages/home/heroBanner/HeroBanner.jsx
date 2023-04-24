e99170740904c7f67aa6fc3a11834fd58a81a0c6
Aaditya Kumar <adityavishwakarma2011@gmail.com>
2023-04-24 18:41:07 +0530

    Fixed Search Button functionality and added search icon in mobile view search bar

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -19,45 +19,51 @@ function HeroBanner() {
   const { data, loading } = useFetch("/movie/upcoming");
 
   useEffect(() => {
-      const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-      // console.log(bg);
-      setBackground(bg);
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    // console.log(bg);
+    setBackground(bg);
   }, [data]);
 
   const searchQueryHandler = (event) => {
-    if(event.key === "Enter" && query.length > 0) {
-       navigate(`/search/${query}`);
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  }
+
+  const searchButtonHandler = () => {
+    if (query.length > 0) {
+      navigate(`/search/${query}`);
     }
   }
 
   return (
     <div className="heroBanner">
 
-    { !loading && <div className="backdrop-img">
-      <Img src={background} />
-    </div> }
+      {!loading && <div className="backdrop-img">
+        <Img src={background} />
+      </div>}
 
-    <div className="opacity-layer">
-      
-    </div>
+      <div className="opacity-layer">
+
+      </div>
 
       <ContentWrapper >
         {/* <div className="wrapper"> */}
-          <div className="heroBannerContent">
-            <span className="title"> Welcome </span>
-            <span className="subTitle"> Millions of movies, TV shows and people to discover. Explore Now !!  </span>
-            <div className="searchInput">
-              <input 
-              type="text" 
+        <div className="heroBannerContent">
+          <span className="title"> Welcome </span>
+          <span className="subTitle"> Millions of movies, TV shows and people to discover. Explore Now !!  </span>
+          <div className="searchInput">
+            <input
+              type="text"
               placeholder="Search for movies or TV shows"
               onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
-              />
-              <button> Search </button>
-            </div>
+            />
+            <button onClick={searchButtonHandler}> Search </button>
           </div>
+        </div>
         {/* </div> */}
-    </ContentWrapper>
+      </ContentWrapper>
     </div>
   );
 }
